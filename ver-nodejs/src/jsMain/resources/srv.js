4f9df0f93bbc034af23f86ed67e318bd049681ee
Михаил Капелько <kornerr@gmail.com>
2025-09-03 22:27:18 +0300

    add CLD patch

--- ver-nodejs/src/jsMain/resources/srv.js
+++ ver-nodejs/src/jsMain/resources/srv.js
@@ -1,9 +1,34 @@
 let http = require("http");
 let KT = require("pskov-ver-nodejs").org.opengamestudio;
 
-KT.tmpHWFun();
+//!<-- Component -->
+
+function SrvComponent() {
+    this._construct = function() {
+        this.ctrl = new KT.CLDController(new KT.SrvContext());
+        // Debugging.
+        this.ctrl.registerCallback((c) => {
+            console.log(`ИГР SrvC._construct ctrl key/value: '${c.recentField}'/'${c.field(c.recentField)}'`);
+        });
+
+        this.setupEffects();
+        this.setupShoulds();
+    };
+
+    this.setupEffects = function() {
+    };
+
+    this.setupShoulds = function() {
+    };
+
+    this._construct();
+}
+
+//<!-- Installation -->
 
 let srv = http.createServer((req, res) => {
+    console.log("ИГР req:", req);
+    console.log("ИГР res:", res);
     res.write("Hello, world. Kotlin is in the console");
     res.end();
 });
