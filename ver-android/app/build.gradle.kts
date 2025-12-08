0220d922dd70648a39ac6a8054b1040827b53ea9
Михаил Капелько <kornerr@gmail.com>
2025-12-08 22:22:24 +0300

    add webkit dep

--- ver-android/app/build.gradle.kts
+++ ver-android/app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.webkit)
 }
 
 kotlin {
